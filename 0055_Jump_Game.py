695cf24115437241f1bb2a0bdd01b3bb7f3e5c4e
seanb <sabrede@ucsd.edu>
2021-07-04 15:38:31 -0700

    initial

--- 0055_Jump_Game.py
+++ 0055_Jump_Game.py
@@ -6,10 +6,13 @@ class Solution:
 
         # while we are within the array and still have steps left
         while i < last_pos:
+            # update the number of steps left, if none then quit
             steps_left = max(nums[i], steps_left)
             if steps_left == 0: return False
 
+            # update our position and reduce the number of steps left
             steps_left -= 1
             i += 1
 
+        # we only get here if we reached the end
         return True
